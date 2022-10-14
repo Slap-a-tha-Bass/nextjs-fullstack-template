2ad2e2665b0887e2abd97f62358fd4c430f2430f
Corey DeLoach <cdeloach16@gmail.com>
2022-10-14 15:57:01 -0500

    fix: primarylayoutmock needs no props

--- src/components/layouts/primary/PrimaryLayout.mocks.ts
+++ src/components/layouts/primary/PrimaryLayout.mocks.ts
@@ -1,18 +1,6 @@
 import { IPrimaryLayout } from './PrimaryLayout';
 
-const PrimaryLayoutMock: IPrimaryLayout = {
-  tag: 'Tag',
-  title: 'Title',
-  body: 'Body',
-  author: 'Author',
-  time: 'Time',
-};
-const AltCardMock: IPrimaryLayout = {
-  tag: 'Alt Tag',
-  title: 'Alt Title',
-  body: 'Alt Body',
-  author: 'Alt Author',
-  time: 'Alt Time',
-};
+const PrimaryLayoutMock: IPrimaryLayout = {};
+const AltCardMock: IPrimaryLayout = {};
 
 export const MockPrimaryLayout = { PrimaryLayoutMock, AltCardMock };
