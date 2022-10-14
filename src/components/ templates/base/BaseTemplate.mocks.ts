aa6a94525d7548b66e95f12a1f116de6c75f20e1
Corey DeLoach <cdeloach16@gmail.com>
2022-10-14 13:41:03 -0500

    feat: added BaseTemplate component

---
+++ src/components/ templates/base/BaseTemplate.mocks.ts
@@ -0,0 +1,10 @@
+import { IBaseTemplate } from './BaseTemplate';
+
+const BaseTemplateMock: IBaseTemplate = {
+  sampleTextProp: 'Hello World',
+};
+const AltTemplateMock: IBaseTemplate = {
+  sampleTextProp: 'Goodbye World',
+};
+
+export const MockBaseTemplate = { BaseTemplateMock, AltTemplateMock };
---
+++ src/components/ templates/base/BaseTemplate.tsx
@@ -0,0 +1,13 @@
+import styles from './BaseTemplate.module.css';
+
+export interface IBaseTemplate {
+  sampleTextProp: string;
+}
+
+export default function BaseTemplate({ sampleTextProp }: IBaseTemplate) {
+  return (
+    <div className={styles.container}>
+      <h1>{sampleTextProp}</h1>
+    </div>
+  );
+}
